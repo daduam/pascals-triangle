6904522a2b80e310d2e520db79a269d98908cd2a
Joseph Ampadu <josephampadu549@gmail.com>
2022-08-23 19:21:14 +0000

    generate project

---
+++ src/main/java/io/github/daduam/pascalstriangle/PascalsTriangleTester.java
@@ -0,0 +1,11 @@
+package io.github.daduam.pascalstriangle;
+
+/**
+ * Pascal's triangle tester
+ *
+ */
+public class PascalsTriangleTester {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/io/github/daduam/pascalstriangle/PascalsTriangleTesterTest.java
@@ -0,0 +1,18 @@
+package io.github.daduam.pascalstriangle;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple PascalsTriangleTester.
+ */
+public class PascalsTriangleTesterTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
